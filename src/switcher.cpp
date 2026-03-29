1a514caa6dbd018da8b48570b1836347c62e6ec1
Björn Kettunen <bjorn.kettunen@thaodan.de>
2026-03-29 02:20:52 +0200

    Switch to GPL-3.0 with SPDX metadata, add LICENSE file

--- src/switcher.cpp
+++ src/switcher.cpp
@@ -1,21 +1,11 @@
-/***************************************************************************
- *   Copyright 2009 by Martin Gräßlin <kde@martin-graesslin.com>           *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
- ***************************************************************************/
+/*  -*- c++ -*-
+ * Copyright 2018 by Tatu Lahtela <lahtela@iki.fi>
+ * SPDX-FileCopyrightText: 2026 Bjorn Kettunen <bjorn.kettunen@thaodan.de>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+
 #include "switcher.h"
 
 #include "switcher_debug.h"
